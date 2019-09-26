b0e1bc4e36ac3c60546e5095d9ccbcf01c3d1d66
Carlos Diaz <cdiaz86@gmail.com>
2019-09-26 14:35:56 -0400

    Outputs list of students missing BMES 201 & BMES 202.

--- student_course_status.rb
+++ student_course_status.rb
@@ -1,12 +1,26 @@
 require 'csv'
 
-current_bmes_students = []
+current_bmes_id = []
 
 CSV.foreach('data/STU-Eligible_To_Register-Fall-09_26_2019.csv', headers: true) do |header|
-  if header["Program 1"] == "BSBE-BMES"
-    current_bmes_students << header["Univ Id"]
-    current_bmes_students << header["Student"]
+  if header["Program 1"] == "BSBE-BMES" && header["Term Code Admit"] != "201915"
+    current_bmes_id << header["Univ Id"]
+    # current_bmes_students << header["Student"]
   end
 end
 
-current_bmes_id_name = Hash[*current_bmes_students]
+current_bmes_id
+
+taken_bmes201_bmes202_ids = []
+
+CSV.foreach('data/STU-Student Transcript_biomed_09_26_2019.csv', headers: true) do |header|
+  if current_bmes_id.include?(header["Univ Id"])
+    if header["Course"].start_with?("BMES 201", "BMES 202")
+      taken_bmes201_bmes202_ids << header["Univ Id"]
+    end
+  end
+end
+
+no_bmes201_bmes202 = current_bmes_id - taken_bmes201_bmes202_ids
+
+p no_bmes201_bmes202
